bd24b76a2f6a82e26cb433c28f8645df85302e38
Oycea <margaret8958100@gmail.com>
2026-02-28 03:40:01 +0300

    Create tokens

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module student-api
 
 go 1.25.0
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/golang-jwt/jwt/v5"
 )
+
+var secret = []byte("secret_key")
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/api/v1/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
+	r.Post("/api/v1/auth/login", loginHandler)
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	var req LoginRequest
+	json.NewDecoder(r.Body).Decode(&req)
+
+	if req.Username != "admin" || req.Password != "admin" {
+		http.Error(w, "invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": req.Username,
+		"exp":      time.Now().Add(24 * time.Hour).Unix(),
 	})
 
-	log.Println("Server started on :8080!")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	tokenString, _ := token.SignedString(secret)
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": tokenString,
+	})
 }
